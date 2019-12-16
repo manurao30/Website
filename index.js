e5bfe901e4241fb0a9271e0030070ab081f5b122
Manish Rao <manurao@g.ucla.edu>
2019-12-16 22:30:55 +0530

    base frontend

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'frontend/build')))
+
 app.get('/api/name', cors(), async(req, res, next) => {
     try {
         console.log('lol')
@@ -10,6 +12,10 @@ app.get('/api/name', cors(), async(req, res, next) => {
     catch(err) {
         next(err)
     }
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
 })
 
 const PORT = process.env.PORT || 3000
